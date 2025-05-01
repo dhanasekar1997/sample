f27756e00dcb76db423c8a42af4e17315b859b6f
dhanasekar1997 <dhanasekar.natarajan01@gmail.com>
2025-05-01 12:25:36 +0530

    Sample2

--- src/main/java/demo/sample1.java
+++ src/main/java/demo/sample1.java
@@ -4,5 +4,6 @@ public class sample1 {
     public static void main(String[] args) {
         System.out.println("Dhanasekar");
         System.out.println("Annamalai");
+        System.out.println("Technoly");
     }
 }
